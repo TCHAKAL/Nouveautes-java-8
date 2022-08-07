7cec1f8595c3a542b707c15e9f450bbe0b4354c2
TCHAKAL <ahmed.tchakal@gmail.com>
2022-08-07 13:29:47 +0100

    Les interfaces fonctionnelles FUNCTION

---
+++ src/main/java/dz/formation/java8/interfaces_fonctionnelles_function/Facture.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dz.formation.java8.interfaces_fonctionnelles_function;
+
+import java.util.function.UnaryOperator;
+
+/**
+ *
+ * @author tchakal.ahmed
+ */
+public class Facture {
+    
+    private String numero;
+
+    Facture(String numero) {
+        this.numero=numero;
+    }
+    
+    public void incrementNumero(UnaryOperator<String> inc){
+        numero = inc.apply(numero);
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+    
+}
