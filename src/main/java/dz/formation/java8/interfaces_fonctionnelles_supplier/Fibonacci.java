261a3c2935951c899f70d37e3832834fb46ba76e
TCHAKAL <ahmed.tchakal@gmail.com>
2022-08-07 14:27:38 +0100

    Les interfaces fonctionnelles Supplier

---
+++ src/main/java/dz/formation/java8/interfaces_fonctionnelles_supplier/Fibonacci.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dz.formation.java8.interfaces_fonctionnelles_supplier;
+
+/**
+ *
+ * @author tchakal.ahmed
+ */
+public class Fibonacci {
+
+    static int[] suite = {1,2,3,4,5,6,7,8,9};
+    static int i=0;
+    static int getAsInt() {
+        return suite[i++];
+    }
+}
---
+++ src/main/java/dz/formation/java8/interfaces_fonctionnelles_supplier/SupplierInterfaceFunctionnelle.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dz.formation.java8.interfaces_fonctionnelles_supplier;
+
+import java.util.function.IntSupplier;
+
+
+/**
+ *
+ * @author tchakal.ahmed
+ */
+public class SupplierInterfaceFunctionnelle {
+
+    public static void main(String[] args) {
+        parcours(Fibonacci::getAsInt);
+    }
+    
+    public static void parcours(IntSupplier f){
+        for(int i = 0 ; i<=5 ;i++){
+            System.out.println("Suivant : "+f.getAsInt());
+        }
+    }
+}
